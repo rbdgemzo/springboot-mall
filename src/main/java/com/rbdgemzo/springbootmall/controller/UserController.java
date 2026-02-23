b0674b05153f87e0eedf1a5e420c289af0404e7d
rbdgemzo <rbdgemzo@gmail.com>
2026-02-23 21:39:32 +0800

    帳號功能 - 登入

--- src/main/java/com/rbdgemzo/springbootmall/controller/UserController.java
+++ src/main/java/com/rbdgemzo/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.rbdgemzo.springbootmall.controller;
 
+import com.rbdgemzo.springbootmall.dto.UserLoginRequest;
 import com.rbdgemzo.springbootmall.dto.UserRegisterRequest;
 import com.rbdgemzo.springbootmall.model.User;
 import com.rbdgemzo.springbootmall.service.UserService;
@@ -23,5 +24,13 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/rbdgemzo/springbootmall/service/UserService.java
+++ src/main/java/com/rbdgemzo/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.rbdgemzo.springbootmall.service;
 
+import com.rbdgemzo.springbootmall.dto.UserLoginRequest;
 import com.rbdgemzo.springbootmall.dto.UserRegisterRequest;
 import com.rbdgemzo.springbootmall.model.User;
 
@@ -7,4 +8,6 @@ public interface UserService {
     User getUserById(Integer userId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
