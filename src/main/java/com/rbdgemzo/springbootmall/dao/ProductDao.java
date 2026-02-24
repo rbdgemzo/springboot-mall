fca7ab638d763cb081f18d6ee4992debdfa7eb84
rbdgemzo <rbdgemzo@gmail.com>
2026-02-24 18:24:06 +0800

    訂單功能 - 檢查 user 是否存在、扣除商品庫存

--- src/main/java/com/rbdgemzo/springbootmall/dao/ProductDao.java
+++ src/main/java/com/rbdgemzo/springbootmall/dao/ProductDao.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface ProductDao {
     Integer countProduct(ProductQueryParams productQueryParams);
+
     List<Product> getProducts(ProductQueryParams productQueryParams);
 
     Product getProductById(Integer productId);
@@ -16,6 +17,8 @@ public interface ProductDao {
     Integer createProduct(ProductRequest productRequest);
 
     void updateProduct(Integer productId, ProductRequest productRequest);
+
+    void updateStock(Integer productId, Integer stock);
 
     void deleteProductById(Integer productId);
 }
